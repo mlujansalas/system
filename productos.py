83a29cad6589b2ee5fdf072fcf872108cd0cb1bf
Marlon Lujan Salas <mlujan.salas@gmail.com>
2022-03-15 14:44:54 -0500

    yaaaa

--- productos.py
+++ productos.py
@@ -1,11 +1,2 @@
 print("probando")
 print("prueba estando en github")
-<<<<<<< HEAD
-=======
-print("probando un conflicto"4
-print("hola mundo")
-<<<<<<< HEAD
->>>>>>> 665de17a1d153a07a094d6c3d604d3e9db1739ad
-=======
-print("enviando archivos del github al git")
->>>>>>> 550bac438ac396e774be13304cc1f68dadd938a6
