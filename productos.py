6d7940d7c0ffc7c7f62aaa7bce9b21a626b7ae46
Marlon Lujan Salas <mlujan.salas@gmail.com>
2022-03-21 17:21:31 -0500

    seguimos probando github

--- productos.py
+++ productos.py
@@ -7,3 +7,5 @@ print (prueba)
 print("enetendiendo github")
 print("probando los datos")
 print("probando nuevo repositorio")
+print("""Seguimos probando git y github,
+ hasta el momento se me complica un poco lo de las llaves publicas""")
